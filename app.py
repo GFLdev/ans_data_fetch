1aa4ae461b91cab69b05ce721c4e952c490f91c8
Gabriel Franco Lourenço <gabriel.franco@gfldev.com>
2024-03-27 16:35:03 -0300

    Added: dbc to dbf

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
+"""
+Script para obtencao dos dados da ANS em .dbc
+e conversao para .dbf
+"""
+
 from bs4 import BeautifulSoup
+from pyreaddbc.readdbc import dbc2dbf
 import requests
 import os
 
@@ -14,25 +20,35 @@ def is_directory(url: str) -> bool:
     else:
         return False
 
-def fetch_data(url: str, out_path: str) -> None:
+def fetch_data(url: str, path: str) -> None:
+    """
+    Obtencao e conversao dos dados
+
+    Args: url (URL para download), path (caminho relativo para salvamento)
+    Ret: None
+    """
     page = requests.get(url, timeout=10).content
     bs_obj = BeautifulSoup(page, "html.parser")
     possible_dirs = bs_obj.findAll("a", href=True)
 
     for link in possible_dirs:
-        if is_directory(link["href"]):
-            curr_dir = os.path.abspath(os.path.join(out_path, link["href"]))
+        if link["href"].startswith("/FTP/"):
+            continue
+        elif is_directory(link["href"]):
+            curr_dir = os.path.abspath(os.path.join(path, link["href"]))
             new_url = url + link["href"]
             os.makedirs(curr_dir, exist_ok=True)
 
             fetch_data(new_url, curr_dir)
         else:
             if link["href"].endswith(".dbc"):
-                out = os.path.abspath(os.path.join(out_path, link["href"]))
-                res = requests.get(f"{url}/{link["href"]}", timeout=10)
+                out = os.path.abspath(os.path.join(path, link["href"]))
+                res = requests.get(f"{url}/{link['href']}", timeout=10)
 
                 open(out, "wb").write(res.content)
-                print(f"{out}: {res.status_code}")
+                dbc2dbf(out, f"{out[:-3]}dbf")
+                os.remove(out)
+                print(f"{out[:-3]}dbf: {res.status_code}")
 
 root_dir = os.getcwd()
 os.makedirs(os.path.abspath(os.path.join(root_dir, "Dados")), exist_ok=True)
