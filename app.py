72d49e6a2c96b6564c3d03be02e101666038c2d3
Gabriel Franco Lourenço <gabrielfranco@agros.org.br>
2024-03-27 11:59:20 -0300

    Added: fetching data complete

--- app.py
+++ app.py
@@ -1,18 +1,43 @@
-import subprocess
+from bs4 import BeautifulSoup
+import requests
+import os
 
-def runcmd(cmd, verbose = False, *args, **kwargs):
-    process = subprocess.Popen(
-        cmd,
-        stdout = subprocess.PIPE,
-        stderr = subprocess.PIPE,
-        text = True,
-        shell = True
-    )
-    std_out, std_err = process.communicate()
-    if verbose:
-        print(std_out.strip(), std_err)
-    pass
+def is_directory(url: str) -> bool:
+    """
+    Verificar se URL e um diretorio
+
+    Arg: url (URL para verificacao)
+    Ret: bool
+    """
+    if url.endswith("/"):
+        return True
+    else:
+        return False
+
+def fetch_data(url: str, out_path: str) -> None:
+    page = requests.get(url, timeout=10).content
+    bs_obj = BeautifulSoup(page, "html.parser")
+    possible_dirs = bs_obj.findAll("a", href=True)
+
+    for link in possible_dirs:
+        if is_directory(link["href"]):
+            curr_dir = os.path.abspath(os.path.join(out_path, link["href"]))
+            new_url = url + link["href"]
+            os.makedirs(curr_dir, exist_ok=True)
+
+            fetch_data(new_url, curr_dir)
+        else:
+            if link["href"].endswith(".dbc"):
+                out = os.path.abspath(os.path.join(out_path, link["href"]))
+                res = requests.get(f"{url}/{link["href"]}", timeout=10)
+
+                open(out, "wb").write(res.content)
+                print(f"{out}: {res.status_code}")
+
+root_dir = os.getcwd()
+os.makedirs(os.path.abspath(os.path.join(root_dir, "Dados")), exist_ok=True)
+root_dir = os.path.abspath(os.path.join(root_dir, "Dados"))
 
 URL_BASE = "https://dadosabertos.ans.gov.br/FTP/Base_de_dados/Microdados/dados_dbc/"
-# Requisição para obtenção dos dados com base nos parâmetros
-runcmd(f"wget -r -np -R 'index.html*' {URL_BASE}", verbose=True)
+
+fetch_data(URL_BASE, root_dir)
